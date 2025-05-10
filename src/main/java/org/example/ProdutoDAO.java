80e9d0dc94ed1b51c77c55933b3c99142507a0a5
matheus maximiano <138736238+theusmaximiano@users.noreply.github.com>
2025-05-10 18:21:39 -0300

    Implementação das operações de listar, pesquisar, alterar e excluir no ProdutoDAO

--- src/main/java/org/example/ProdutoDAO.java
+++ src/main/java/org/example/ProdutoDAO.java
@@ -1,6 +1,16 @@
 package org.example;
 
+import java.util.Collection;
+
 public interface ProdutoDAO {
 
     void insere(Produto produto);
+
+    Produto buscaPorId(Integer id);
+
+    void altera(Produto produto);
+
+    void exclui(Integer id);
+
+    Collection<Produto> lista();
 }
